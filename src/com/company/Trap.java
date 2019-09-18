332c499578f76b73c3f97d09c08cd694957f2f53
Henrik Björk <bjork.henrik@gmail.com>
2019-09-18 14:52:19 +0200

    the trap class

---
+++ src/com/company/Trap.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Trap {
+    private String name;
+    private int damage;
+
+    public Trap(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public int hurtPlayer() {
+      if((damage) <= 0) {
+          return 0;
+      }
+      return 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public String toString() {
+        return "Trap{" +
+                "name='" + name + '\'' +
+                ", damage=" + damage +
+                '}';
+    }
+}
